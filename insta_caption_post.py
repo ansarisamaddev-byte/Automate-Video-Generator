15ed2b72b8ff2e378b7182b2db5d5ce8a9ca7bdf
AstixX <ansari.samad.dev@gmail.com>
2026-05-05 10:30:15 -0700

    Update insta_caption_post.py

--- insta_caption_post.py
+++ insta_caption_post.py
@@ -16,13 +16,17 @@ SCREEN_W, SCREEN_H = 1080, 1920
 SAFE_MARGIN = 60
 SLIDE_OFFSET = 80
 FONTS = [
-    r"fonts/dejavu-sans-bold.ttf",
-    r"fonts/New folder/Montserrat-VariableFont_wght.ttf",
-    r"fonts/Blankit-8MW2B.ttf", r"fonts/Cintaly-ax7v9.otf",
-    r"fonts/dejavu-sans-bold.ttf", r"fonts/Sabering-gwAG3.otf",
-    r"fonts/Sugiono-3zqyy.ttf", r"fonts/SukaCoffee-DYmzE.otf"
-     # Ensure these paths are correct
+    r"fonts\Blackburn Free.ttf",
+    r"fonts\Blankit-8MW2B.ttf",
+    r"fonts\Cintaly-Eazdl.ttf",
+    r"fonts\dejavu-sans-bold.ttf",
+    r"fonts\LemonJellyPersonalUse-dEqR.ttf",
+    r"fonts\NotoColorEmoji.ttf",
+    r"fonts\PoemLettersSerifRegular-lgggX.ttf",
+    r"fonts\Sugiono-3zqyy.ttf",
+    r"fonts\WorldstarRegular.ttf"
 ]
+
 
 # ---------------- UTILS ---------------- #
 def make_even(x):
