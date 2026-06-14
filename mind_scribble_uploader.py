a644f5c1df44fd79048ee629902c3761e9395c4d
Samad Ansari <ansari.samad.dev@gmail.com>
2026-06-14 14:42:45 +0530

    changes

--- mind_scribble_uploader.py
+++ mind_scribble_uploader.py
@@ -37,10 +37,11 @@ def get_active_style():
     """Returns the style name based on current time."""
     hour = datetime.datetime.now().hour
     # Define your time windows
-    if 6 <= hour < 18:
-        return "morning"
-    else:
-        return "night"
+    return "night"
+    # if 6 <= hour < 18:
+    #     return "morning"
+    # else:
+    #     return "night"
 
 def get_service():
     creds = None
