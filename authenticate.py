f35de912469eeb4506caa4ef0e28041ffa39f3cc
unknown <ansari.samad@dev.com>
2026-07-05 00:40:43 +0530

    changes

--- authenticate.py
+++ authenticate.py
@@ -2,13 +2,13 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 import json
 
 # Point this to the JSON file you just downloaded
-CLIENT_SECRETS_FILE = 'coldcases_client_secret.json' 
+CLIENT_SECRETS_FILE = 'client_secret_mindscribble.json' 
 SCOPES = ['https://www.googleapis.com/auth/youtube.upload']
 
 flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
-# This will open a browser for you to log in to your @ColdCases-pov account
+# This will open a browser for you to log in to your @Mindscribble account
 credentials = flow.run_local_server(port=0)
 
 # Save the resulting credentials to a new file
-with open('coldcases_auth.json', 'w') as f:
+with open('mindscribble_auth.json', 'w') as f:
     f.write(credentials.to_json())
